cbee4e9a81e416d7e5fba890309934badcc8f21d
julian so <julianso89@gmail.com>
2018-11-19 19:58:59 -0800

    feature set 1 complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,12 @@
+import React from "react";
+import './app.css';
 
+function App() {
+  return (
+    <div className="main-container">
+      <h1 className="main-title">Scrapbook</h1>
+    </div>
+  );
+}
 
+export default App;
