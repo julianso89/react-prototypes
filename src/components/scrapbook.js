c9ed8564bc3202d15d3c1ba63d1d05538535d8b8
julian so <julianso89@gmail.com>
2018-11-19 20:09:59 -0800

    feature set 2 complete

---
+++ src/components/scrapbook.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Scrapbook from "./scrapbook.css";
+import humming from "./images/humming.jpg";
+
+export default props => {
+  return (
+    <div className="scrapbook-container">
+      <img src={humming} />
+    </div>
+  );
+};
