d00d063f7843a01358cf3d317fbe9ba7fe24f5b8
julian so <julianso89@gmail.com>
2018-11-21 16:19:56 -0800

    movie and movie container components created

---
+++ src/components/movie_container.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Movies from "./movies";
+
+class MovieContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Movie Container</h2>
+        <Movie />
+      </div>
+    );
+  }
+}
+
+export default MovieContainer;
